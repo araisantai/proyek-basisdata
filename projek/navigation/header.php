e4c8d086c328442864332522f62dee54e84d75a5
athmuf <72547188+athmuf@users.noreply.github.com>
2021-12-20 20:14:07 +0700

    Update header.php

--- projek/navigation/header.php
+++ projek/navigation/header.php
@@ -13,6 +13,7 @@ include "config.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/liststyle.css">
+    <link rel="stylesheet" href="css/index-style.css">
     <title>Agri Helper</title>
 </head>
 <body>
