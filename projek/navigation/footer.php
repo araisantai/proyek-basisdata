252e694e02ffc4f3d371fc6cd4e7541f44b36117
farissbayu <95034975+farissbayu@users.noreply.github.com>
2021-12-20 19:40:04 +0700

    Update footer.php

--- projek/navigation/footer.php
+++ projek/navigation/footer.php
@@ -1,6 +1,6 @@
      <!-- footer -->
     <footer class="text-white text-center fixed-bottom" style = "padding-top : 8px; background-color : #0ab68b">
-      <p style = "font-size : 16px">2021 - Kelompok </p>
+      <p style = "font-size : 16px">2021 - Kelompok 9</p>
     </footer>
     <!-- akhir footer -->
 
