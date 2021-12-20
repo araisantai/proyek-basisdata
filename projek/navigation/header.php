146027bc7f5a78241b999dfddbac16f5345c8e26
farissbayu <95034975+farissbayu@users.noreply.github.com>
2021-12-20 19:47:03 +0700

    Update header.php

--- projek/navigation/header.php
+++ projek/navigation/header.php
@@ -13,13 +13,13 @@ include "config.php"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/liststyle.css">
-    <title>Agri Helpher</title>
+    <title>Agri Helper</title>
 </head>
 <body>
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark" style = "background-color: #0ab68b">
     <div class="container-fluid">
-    <a class="navbar-brand" href="#">Agri Helpher</a>
+    <a class="navbar-brand" href="#">Agri Helper</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
