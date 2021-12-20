cfed53eec96fc6f385088761e38c25fe257341a5
athmuf <72547188+athmuf@users.noreply.github.com>
2021-12-20 09:53:34 +0700

    Update header.php

--- projek/navigation/header.php
+++ projek/navigation/header.php
@@ -10,6 +10,9 @@ include "config.php"
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/liststyle.css">
     <title>Projek Basdat</title>
 </head>
 <body>
